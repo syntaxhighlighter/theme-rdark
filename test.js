68b1c72e536c801aa5172511a51c0386b1a98c22
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 08:54:46 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Removes Grunt
    * Removes globals and window references
    * Uses Babel

---
+++ test.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var sass = require('node-sass');
+
+describe('theme-rdark', function() {
+  it('compiles ok', function(done) {
+    fs.readFile(`${__dirname}/theme.scss`, 'utf8', function(err, content) {
+      var results = sass.renderSync({
+        data: content,
+        includePaths: [`${__dirname}/node_modules/theme-base`]
+      });
+      if(!results.css.length) throw new Error("Expecting results");
+      done();
+    });
+  });
+});
